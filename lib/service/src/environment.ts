2a7f6ccd684b0fb7d14a28ee5257149938788a5a
Daniel Huth <daniel.huth@pickware.de>
2022-03-19 09:33:23 +0000

    Fix env parsing

--- lib/service/src/environment.ts
+++ lib/service/src/environment.ts
@@ -1,9 +1,10 @@
 import * as t from "io-ts";
+import { BooleanFromString } from "io-ts-types";
 
 export const EnvironmentStructure = t.type({
     SENTRY_DSN: t.string,
     SENTRY_ENVIRONMENT: t.string,
-    PRETTY_LOGGING: t.boolean
+    PRETTY_LOGGING: BooleanFromString
 });
 
 export type Environment = t.TypeOf<typeof EnvironmentStructure>;
