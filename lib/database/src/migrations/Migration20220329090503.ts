b372186496912568b2d1aa5f0e9df8e70211431b
Daniel Huth <daniel.huth@pickware.de>
2022-04-15 12:41:12 +0200

    Add docker compose build to ci

--- lib/database/src/migrations/Migration20220329090503.ts
+++ lib/database/src/migrations/Migration20220329090503.ts
@@ -5,13 +5,14 @@ export class Migration20220329090503 extends Migration {
   async up(): Promise<void> {
     this.addSql(`
       alter table "user" add column "interested_in_sources" text[] not null default '{
-        "steam",
-        "switch",
-        "psStore",
-        "epic",
-        "metacritic",
+        steam,
+        switch,
+        psStore,
+        epic,
+        metacritic,
       }';
     `);
+
     this.addSql('alter table "user" add column "country" varchar(255) not null default \'de-DE\';');
     this.addSql('create index "notification_read_index" on "notification" ("read");');
   }
