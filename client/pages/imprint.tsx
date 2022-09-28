13a3fc55a6f729aa3a72ce86f6a2f3d1ca00e256
Daniel Huth <4455258+Agreon@users.noreply.github.com>
2022-09-28 15:33:31 +1300

    Remove phone number

--- client/pages/imprint.tsx
+++ client/pages/imprint.tsx
@@ -1,5 +1,5 @@
-import { Box, Text } from "@chakra-ui/react";
-import { NextPage } from "next";
+import { Box, Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
 
 const Legal: NextPage = () => {
     return (
@@ -7,12 +7,11 @@ const Legal: NextPage = () => {
             <Text fontSize="2xl">Imprint</Text>
 
             <p>Daniel Huth<br />
-            Im Langgarten 1<br />
-            63589 Linsengericht</p>
+                Im Langgarten 1<br />
+                63589 Linsengericht</p>
 
             <Text fontSize="xl" mt="1rem">Contact</Text>
-            <p>Phone: +49 (0) 1575 2474713<br />
-            Mail: huth at duck.com</p>
+            <p>Mail: huth at duck dot com</p>
         </Box>
     );
 };
