e354f4420d34f32b4ecf82be959c9f7d226fab93
Daniel Huth <4455258+Agreon@users.noreply.github.com>
2023-01-16 09:49:50 +1300

    Add source logos for select

--- client/components/InfoSource/AddInfoSource.tsx
+++ client/components/InfoSource/AddInfoSource.tsx
@@ -1,11 +1,36 @@
-import { Box, Button, Flex, Input, Select } from '@chakra-ui/react';
-import { InfoSourceType, InfoSourceTypeNames } from '@game-watch/shared';
-import { useState } from 'react';
+import { Box, Button, Flex, Input } from '@chakra-ui/react';
+import { InfoSourceType } from '@game-watch/shared';
+import {
+    chakraComponents,
+    OptionBase,
+    OptionProps,
+    Select,
+    SingleValueProps,
+} from 'chakra-react-select';
+import { useMemo, useState } from 'react';
 
 import { useGameContext } from '../../providers/GameProvider';
 import { useUserContext } from '../../providers/UserProvider';
 import { SourceUrlPlaceholder } from '../../util/source-url-placeholder';
 import { useAction } from '../../util/useAction';
+import { SourceTypeLogo } from './SourceTypeLogo';
+
+interface InfoSourceOption extends OptionBase {
+    value: InfoSourceType;
+}
+
+const customComponents = {
+    Option: ({ ...props }: OptionProps<InfoSourceOption>) => (
+        <chakraComponents.Option {...props}>
+            {SourceTypeLogo[props.data.value]}
+        </chakraComponents.Option>
+    ),
+    SingleValue: ({ ...props }: SingleValueProps<InfoSourceOption>) => (
+        <chakraComponents.SingleValue {...props}>
+            {SourceTypeLogo[props.data.value]}
+        </chakraComponents.SingleValue>
+    )
+};
 
 export const AddInfoSource: React.FC<{
     scheme: 'primary' | 'secondary'
@@ -18,7 +43,12 @@ export const AddInfoSource: React.FC<{
         type => activeInfoSources.find(source => source.type === type) === undefined
     );
 
-    const [type, setType] = useState(availableInfoSources[0] ?? '');
+    const sourceOptions = useMemo(
+        () => availableInfoSources.map(type => ({ value: type, })),
+        [availableInfoSources]
+    );
+
+    const [type, setType] = useState(sourceOptions[0]);
     const [url, setUrl] = useState('');
 
     const { loading, execute: onAdd } = useAction(addInfoSource, {
@@ -30,27 +60,26 @@ export const AddInfoSource: React.FC<{
 
     return (
         <Flex direction={['column', 'row']}>
-            <Box mr="1rem" mb={['0.5rem', 0]}>
-                <Select
-                    pt="0.125rem"
-                    onChange={event => setType(event.target.value as InfoSourceType)}
-                >
-                    {availableInfoSources.map(type => (
-                        <option key={type} value={type}>{InfoSourceTypeNames[type]}</option>
-                    ))}
-                </Select>
+            <Box mr="1rem" mb={['0.5rem', 0]} minWidth="13rem">
+                <Select<InfoSourceOption>
+                    isSearchable={false}
+                    options={sourceOptions}
+                    components={customComponents}
+                    value={type}
+                    onChange={value => value && setType(value)}
+                />
             </Box>
             <Box flex="1" mr="1rem" mb={['0.5rem', 0]}>
                 <Input
                     value={url}
                     disabled={loading}
-                    placeholder={SourceUrlPlaceholder(type, userCountry)}
+                    placeholder={SourceUrlPlaceholder(type.value, userCountry)}
                     onChange={event => setUrl(event.target.value)}
                 />
             </Box>
             <Box>
                 <Button
-                    onClick={() => onAdd({ type, url })}
+                    onClick={() => onAdd({ type: type.value, url })}
                     disabled={loading || !url.length}
                     isLoading={loading}
                     colorScheme={scheme == 'primary' ? 'teal' : undefined}
