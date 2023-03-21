518ea9a72dc2ee742e0f8f9db4f670de39a88f19
Daniel Huth <4455258+Agreon@users.noreply.github.com>
2023-03-21 07:57:14 +0100

    Improve deployment

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 cp ./.env client/.env.local
 # We want to create a good cache for the following images to skip installing libs and puppeteer two times
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 dc build searcher client
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 dc build searcher
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 dc build
 dc up -d
