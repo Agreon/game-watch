22e7960f33b24edef323b6db236b0b6fd2adb143
Daniel Huth <4455258+Agreon@users.noreply.github.com>
2023-04-28 11:47:13 +0200

    Update dependencies

--- lib/shared/src/util/is-non-specific-date.ts
+++ lib/shared/src/util/is-non-specific-date.ts
@@ -1,4 +1,5 @@
 // At least steam returns dates in unspecific ranges like checked below.
+// eslint-disable-next-line max-len
 const NonSpecificDateRegex = /^(Winter |Summer |Spring |Fall |Jan |Feb |Mar |Apr |Mai |Jun |Jul |Aug |Sep |Oct |Nov |Dec )?(20(\d\d))$/;
 
 export const isNonSpecificDate = (date: string) =>
